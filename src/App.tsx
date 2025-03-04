c5608201498c27126cac84a132bb4cc1bd655054
Aaron Bartholomew <abartholomew@blixhost.com>
2025-03-04 15:32:07 -0500

    Dead code removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,22 @@
 import { useRef, useState } from 'react';
 import { IRefPhaserGame, PhaserGame } from './game/PhaserGame';
-import { MainMenu } from './game/scenes/MainMenu';
 import { Game } from './game/scenes/Game';
 import { GameConfig } from './game/config/GameConfig';
 import { CoordinateUtils } from './game/utils/CoordinateUtils';
 
 function App()
 {
-    // The sprite can only be moved in the MainMenu Scene
-    const [canMoveSprite, setCanMoveSprite] = useState(true);
     // State to track if debug panel is collapsed
     const [isDebugCollapsed, setIsDebugCollapsed] = useState(false);
 
     //  References to the PhaserGame component (game and scene are exposed)
     const phaserRef = useRef<IRefPhaserGame | null>(null);
-    const [spritePosition, setSpritePosition] = useState({ x: 0, y: 0 });
 
     const [debugInfo, setDebugInfo] = useState({
         coords: { x: 0, y: 0 },
         chunkCoords: { x: 0, y: 0 },
         tileType: { name: '', color: 0, isWall: false }
     });
-
-    const changeScene = () => {
-
-        if(phaserRef.current)
-        {     
-            const scene = phaserRef.current.scene as MainMenu;
-            
-            if (scene)
-            {
-                scene.changeScene();
-            }
-        }
-    }
-
-    const moveSprite = () => {
-
-        if(phaserRef.current)
-        {
-
-            const scene = phaserRef.current.scene as MainMenu;
-
-            if (scene && scene.scene.key === 'MainMenu')
-            {
-                // Get the update logo position
-                scene.moveLogo(({ x, y }) => {
-
-                    setSpritePosition({ x, y });
-
-                });
-            }
-        }
-
-    }
-
-    const addSprite = () => {
-
-        if (phaserRef.current)
-        {
-            const scene = phaserRef.current.scene;
-
-            if (scene)
-            {
-                // Add more stars
-                const x = Phaser.Math.Between(64, scene.scale.width - 64);
-                const y = Phaser.Math.Between(64, scene.scale.height - 64);
-    
-                //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
-                const star = scene.add.sprite(x, y, 'star');
-    
-                //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
-                //  You could, of course, do this from within the Phaser Scene code, but this is just an example
-                //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
-                scene.add.tween({
-                    targets: star,
-                    duration: 500 + Math.random() * 1000,
-                    alpha: 0,
-                    yoyo: true,
-                    repeat: -1
-                });
-            }
-        }
-    }
 
     // Event emitted from the PhaserGame component
     const currentScene = (scene: Phaser.Scene) => {
@@ -106,16 +40,6 @@ function App()
             }, 100);
 
             return () => clearInterval(updateInterval);
-        }
-        setCanMoveSprite(scene.scene.key === 'MainMenu');
-    }
-
-    const loadScene = (sceneName: string) => {
-        if (phaserRef.current?.scene) {
-            const currentScene = phaserRef.current.scene.scene.key;
-            if (currentScene !== sceneName) {
-                phaserRef.current.scene.scene.start(sceneName);
-            }
         }
     }
 
